c2b6a0d77e4a26d0e806a65bcce69e57d36c338e
yunuchoi <yoonwoochoi00@gmail.com>
2024-07-10 17:43:45 +1200

    Add middleware

--- mis/serializers.py
+++ mis/serializers.py
@@ -4,7 +4,7 @@ from mis.models import User, Course
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username','password']
+        fields = ['id', 'username', 'password']
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
