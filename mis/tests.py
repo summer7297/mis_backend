3014a608a27fff7e7bfc239aa9752f742d683524
yunuchoi <yoonwoochoi00@gmail.com>
2024-07-10 19:55:37 +1200

    Add test cases

--- mis/tests.py
+++ mis/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginViewTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create(username='test_username', password='test_password')
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_success(self):
+        response = self.client.post('/login/', {'username': 'test_username', 'password': 'test_password'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_invalid_credentials(self):
+        response = self.client.post('/login/', {'username': 'test_username', 'password': 'wrong_password'})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_login_user_not_found(self):
+        response = self.client.post('/login/', {'username': 'wrong_username', 'password': 'wrong_password'})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- mis/urls.py
+++ mis/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from mis.viewsets import UserViewSets, CourseViewSets
+from mis.views import login_view
 
 router = DefaultRouter()
 router.register('users', UserViewSets, basename='users')
 router.register('courses', CourseViewSets, basename='courses')
 urlpatterns = router.urls
+urlpatterns += [
+    path('api/login/', login_view, name='login'),
+]
